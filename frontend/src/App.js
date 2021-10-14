eff04d1c3e6a6ad03886f616ec146d503b7c429d
McKayla Lankau <lankaukk@yahoo.com>
2021-10-14 12:31:05 -0700

    app.js to get data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  }
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  // // one of the advantages of using Axios over fetch() is that it onverts the response to JSON automatically
+  loginStatus = () => {
+      axios.get('http://localhost:3001/logged_in', 
+      {withCredentials: true})    
+    .then(response => {
+        if (response.data.logged_in) {
+          this.handleLogin(response)
+        } else {
+          this.handleLogout()
+        }
+      })
+    .catch(error => console.log('api errors:', error))
+  }
+
+  handleLogin = (data) => {
+      this.setState({
+        isLoggedIn: true,
+        user: data.user
+      })
+    }
+
+  handleLogout = () => {
+      this.setState({
+      isLoggedIn: false,
+      user: {}
+      })
+    }
+  
+  render() {
+    return (
+      <div>
+         <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={}/>
+            <Route exact path='/login' component={}/>
+            <Route exact path='/signup' component={}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
