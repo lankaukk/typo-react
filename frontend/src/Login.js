6b6a8d9da45fcf5a85521e42cedf6e571b3df2be
McKayla Lankau <lankaukk@yahoo.com>
2021-10-14 14:44:41 -0700

    initial set up of user authentcation system

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -22,7 +22,42 @@ class Login extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-    };
+        const {username, email, password} = this.state
+        let user = {
+          username: username,
+          email: email,
+          password: password
+        }
+        
+        axios.post('http://localhost:3001/login', {user}, {withCredentials: true})
+            .then(response => {
+            if (response.data.logged_in) {
+                this.props.handleLogin(response.data)
+                this.redirect()
+            } else {
+                this.setState({
+                    errors: response.data.errors
+                })
+            }
+            })
+            .catch(error => console.log('api errors:', error))
+      };
+
+    redirect = () => {
+        this.props.history.push('/')
+      }
+      
+    handleErrors = () => {
+        return (
+          <div>
+            <ul>
+            {this.state.errors.map(error => {
+            return <li key={error}>{error}</li>
+              })}
+            </ul>
+          </div>
+        )
+      };
 
     render() {
         const {username, email, password} = this.statereturn (
@@ -50,7 +85,7 @@ class Login extends Component {
                 value={password}
                 onChange={this.handleChange}
             />   
-                  
+
             <button placeholder="submit" type="submit">
                 Log In
             </button>   
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -22,7 +22,42 @@ class Signup extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
+        const {username, email, password, password_confirmation} = this.state
+        let user = {
+          username: username,
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation
+        }
+
+        axios.post('http://localhost:3001/users', {user}, {withCredentials: true})
+            .then(response => {
+            if (response.data.status === 'created') {
+                this.props.handleLogin(response.data)
+                this.redirect()
+            } else {
+                this.setState({
+                errors: response.data.errors
+                })
+            }
+            })
+            .catch(error => console.log('api errors:', error))
     };
+
+    redirect = () => {
+        this.props.history.push('/')
+      }
+
+    handleErrors = () => {
+        return (
+          <div>
+            <ul>{this.state.errors.map((error) => {
+              return <li key={error}>{error}</li>
+            })}
+            </ul> 
+          </div>
+        )
+      };
 
     render() {
         const {username, email, password, password_confirmation} = this.state
